49cf87ad0f72a100df96bd588664bcffb7769b33
wind <fwindpeak@gmail.com>
2019-11-07 16:02:58 +0800

    增加config server工程

---
+++ spring-cloud/employee-config-server/src/test/java/com/fwindpeak/cloud/employeeconfigserver/EmployeeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwindpeak.cloud.employeeconfigserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
