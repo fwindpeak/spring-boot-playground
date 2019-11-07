6968a5ab109008d51dbf18541d357a803c141090
wind <fwindpeak@gmail.com>
2019-11-07 20:31:54 +0800

    增加spring-boot-admin-server工程

---
+++ spring-cloud/admin-server/src/test/java/com/fwindpeak/cloud/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwindpeak.cloud.adminserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
